288ef430bd20f9d3efffb5b8bf71aa771bf17267
shishiro <dheeravathshishiro@gmail.com>
2023-06-29 13:27:56 +0530

    made some changes

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -3,6 +3,8 @@ import anime from "./contact.json";
 import Lottie from "react-lottie";
 import "../css/contactus.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import emailjs from '@emailjs/browser';
+
 import {
   faPhone,
   faEnvelope,
@@ -27,172 +29,189 @@ function ContactUs() {
   function handleClick() {
     playAnime();
   }
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_dps96mw', 'template_45kap8j', form.current, 'oNdXBCcj3ey7b-Yhp')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    e.target.reset();
+  };
+
   return (
-    <section className="section">
-      <div style={{ display: "flex", justifyContent: "space-around" }}>
-        <div style={{ margin: "auto 10px" }}>
-          <Lottie
-            ref={lottieRef}
-            options={defaultOptions}
-            height={250}
-            width={250}
-          />
-          <div style={{ margin: "10px" }}>
-            <div
-              style={{
-                display: "flex",
-                maxWidth: "700px",
-                background: "whitesmoke",
-                boxShadow: "2px 5px 10px rgba(0,0,0,0.5)",
-                height: "40px",
-                padding: "15px 20px",
-                borderRadius: "5px",
-              }}
-            >
-              <button
+
+    <section className="section" >
+      <form ref={form} onSubmit={sendEmail}>
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          <div style={{ margin: "auto 10px" }}>
+            <Lottie
+              ref={lottieRef}
+              options={defaultOptions}
+              height={250}
+              width={250}
+            />
+            <div style={{ margin: "10px" }}>
+              <div
                 style={{
-                  background: "gray",
-                  borderRadius: "50%",
-                  border: "none",
-                  height: "25px",
-                  width: "25px",
+                  display: "flex",
+                  maxWidth: "700px",
+                  background: "whitesmoke",
+                  boxShadow: "2px 5px 10px rgba(0,0,0,0.5)",
+                  height: "40px",
+                  padding: "15px 20px",
+                  borderRadius: "5px",
                 }}
               >
-                <FontAwesomeIcon icon={faPhone} />
-              </button>
-              <p style={{ margin: "5px" }}>+91 81212 78087</p>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                maxWidth: "700px",
-                background: "whitesmoke",
-                boxShadow: "2px 5px 10px rgba(0,0,0,0.5)",
-                margin: "20px auto",
-                height: "40px",
-                padding: "15px 20px",
-                borderRadius: "5px",
-              }}
-            >
-              <button
+                <button
+                  style={{
+                    background: "gray",
+                    borderRadius: "50%",
+                    border: "none",
+                    height: "25px",
+                    width: "25px",
+                  }}
+                >
+                  <FontAwesomeIcon icon={faPhone} />
+                </button>
+                <p style={{ margin: "5px" }}>+91 81212 78087</p>
+              </div>
+              <div
                 style={{
-                  background: "gray",
-                  borderRadius: "50%",
-                  border: "none",
-                  height: "25px",
-                  width: "25px",
+                  display: "flex",
+                  maxWidth: "700px",
+                  background: "whitesmoke",
+                  boxShadow: "2px 5px 10px rgba(0,0,0,0.5)",
+                  margin: "20px auto",
+                  height: "40px",
+                  padding: "15px 20px",
+                  borderRadius: "5px",
                 }}
               >
-                <FontAwesomeIcon icon={faEnvelope} />{" "}
-              </button>
-              <p style={{ margin: "5px" }}>LCS2022014@iiitl.ac.in</p>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                maxWidth: "700px",
-                background: "whitesmoke",
-                boxShadow: "2px 5px 10px rgba(0,0,0,0.5)",
-                margin: "20px auto",
-                height: "40px",
-                padding: "15px 20px",
-                borderRadius: "5px",
-              }}
-            >
-              <button
+                <button
+                  style={{
+                    background: "gray",
+                    borderRadius: "50%",
+                    border: "none",
+                    height: "25px",
+                    width: "25px",
+                  }}
+                >
+                  <FontAwesomeIcon icon={faEnvelope} />{" "}
+                </button>
+                <p style={{ margin: "5px" }}>LCS2022014@iiitl.ac.in</p>
+              </div>
+              <div
                 style={{
-                  background: "gray",
-                  borderRadius: "50%",
-                  border: "none",
-                  height: "25px",
-                  width: "25px",
+                  display: "flex",
+                  maxWidth: "700px",
+                  background: "whitesmoke",
+                  boxShadow: "2px 5px 10px rgba(0,0,0,0.5)",
+                  margin: "20px auto",
+                  height: "40px",
+                  padding: "15px 20px",
+                  borderRadius: "5px",
                 }}
               >
-                <FontAwesomeIcon icon={faLocationDot} />{" "}
-              </button>
-              <p style={{ margin: "5px" }}>
-                IIIT Lucknow,chak Ganjaraia,UTTARPRADESH
-              </p>
+                <button
+                  style={{
+                    background: "gray",
+                    borderRadius: "50%",
+                    border: "none",
+                    height: "25px",
+                    width: "25px",
+                  }}
+                >
+                  <FontAwesomeIcon icon={faLocationDot} />{" "}
+                </button>
+                <p style={{ margin: "5px" }}>
+                  IIIT Lucknow,chak Ganjaraia,UTTARPRADESH
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div
-          style={{
-            borderRadius: "5px",
-            padding: "20px 15px",
-            width: "500px",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <h1>Contact Us</h1>
-          <div style={{ display: "flex" }}>
-            <div className="input-group">
-              <label className="label">First Name</label>
-              <input
-                required=""
-                type="text"
-                name="text"
-                placeholder="FirstName"
-                className="firstName"
-              />
+          <div
+            style={{
+              borderRadius: "5px",
+              padding: "20px 15px",
+              width: "500px",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <h1>Contact Us</h1>
+            <div style={{ display: "flex" }}>
+              <div className="input-group">
+                <label className="label" htmlFor="
+              user_name">First Name</label>
+                <input
+                  required=""
+                  type="text"
+                  name="user_name"
+                  placeholder="FirstName"
+                  className="firstName"
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="user_lastName" className="label">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  id="user_lastName"
+                  className="firstName"
+                  placeholder="LastName"
+                />
+              </div>
             </div>
             <div className="input-group">
-              <label htmlFor="lastName" className="label">
-                Last Name
+              <label htmlFor="user_email" className="label">
+                Email
               </label>
               <input
-                type="text"
-                name="lastName"
-                id="lastName"
-                className="firstName"
-                placeholder="LastName"
+                type="email"
+                name="user_email"
+                placeholder="Email"
+                className="firstName-active"
               />
             </div>
-          </div>
-          <div className="input-group">
-            <label htmlFor="name" className="label">
-              Email
-            </label>
-            <input
-              type="email"
-              placeholder="Email"
-              className="firstName-active"
-            />
-          </div>
-          <div>
-            <label htmlFor="number" className="label">
-              Phone
-            </label>
-            <input
-              type="number"
-              name="number"
-              id="number"
-              placeholder="Phone"
-              className="firstName-active"
-              style={{ MozAppearance: "textfield" }}
-            />
-          </div>
-          <div>
-            <label htmlFor="complaint" className="label">
-              Describe Your issue
-            </label>
-            <textarea
-              style={{ resize: "none" }}
-              name="complaint"
-              id="complaint"
-              cols="30"
-              rows="10"
-              className="textarea"
-              placeholder="Describe your issue "
-            ></textarea>
-          </div>
+            <div>
+              <label htmlFor="user_phone" className="label">
+                Phone
+              </label>
+              <input
+                type="number"
+                name="user_phone"
+                id="number"
+                placeholder="Phone"
+                className="firstName-active"
+                style={{ MozAppearance: "textfield" }}
+              />
+            </div>
+            <div>
+              <label htmlFor="message" className="label">
+                Describe Your issue
+              </label>
+              <input
+                style={{ resize: "none" }}
+                name="message"
+                id="complaint"
+                // cols="30"
+                // rows="10"
+                className="textarea"
+                placeholder="Describe your issue "
+              />
+            </div>
 
-          <button className="btn" onClick={handleClick}>
-            send
-          </button>
+            <button className="btn" onClick={handleClick}>
+              send
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </section>
   );
 }
--- src/pages/stats.jsx
+++ src/pages/stats.jsx
@@ -1,11 +1,44 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import "../css/stats.css";
+import Play from "./play.jsx"
 export default function Stats() {
+
+  useEffect(() => {
+    fetch('http://localhost:3030/gameData')
+      .then((res) => {
+        return res.json();
+      }).then(data => {
+        console.log(data)
+      })
+  }, [])
+  const rounds = 1;
+  const totalWordsTyped = 30;
+  const accuracy = 100;
+  const wpm = 30;
   return (
-    <div>
-      <h1>STATS</h1>
-
+    <div className="subpage-container">
+      {/* <Play /> */}
+      <div className="stats-section">
+        <h1 className="stats-heading">STATS</h1>
+        <div className="stats-grid">
+          <div className="stats-item">
+            <h2 className="stats-title">Rounds Completed:</h2>
+            <p className="stats-value">{rounds}</p>
+          </div>
+          <div className="stats-item">
+            <h2 className="stats-title">Total Words Typed:</h2>
+            <p className="stats-value">{totalWordsTyped}</p>
+          </div>
+          <div className="stats-item">
+            <h2 className="stats-title">Accuracy:</h2>
+            <p className="stats-value">{accuracy.toFixed(2)}%</p>
+          </div>
+          <div className="stats-item">
+            <h2 className="stats-title">Words Per Minute (WPM):</h2>
+            <p className="stats-value">{wpm}</p>
+          </div>
+        </div>
+      </div>
     </div>
-
   );
 }
